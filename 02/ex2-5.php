e80381d03c2d9ba3addcda1707342438430c8a67
AsDf0102 <choejuho0102@gmail.com>
2023-08-06 16:11:44 +0900

    1주차 스터디

---
+++ 02/ex2-5.php
@@ -0,0 +1,14 @@
+<?php      
+    $kor = 80; // 국어 성적
+    $eng = 90; // 영어 성적
+    $math = 100; // 수학 성적
+
+    $sum = $kor + $eng + $math; // 합계
+    $avg = $sum/3; // 평균
+    
+    echo "국어 : $kor.<br>";
+    echo "영어 : $eng.<br>";
+    echo "수학 : $math.<br>";
+    echo "- 합계 : $sum<br>";
+    echo "- 평균 : $avg<br>";
+?>
