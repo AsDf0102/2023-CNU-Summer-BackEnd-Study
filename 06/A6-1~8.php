53fd7ccfdaa120ac478ee1e52bef1a8ddc189db3
AsDf0102 <choejuho0102@gmail.com>
2023-08-12 09:59:49 +0900

    2주차 스터디

---
+++ 06/A6-1~8.php
@@ -0,0 +1,13 @@
+<?php
+  /*
+    1 : setcookie()
+    2 : isset()
+    3 : $_COOKIE
+    4 : 쿠키 -> 클라이언트 저장, 보안 취약, 빠름, 지속 
+        세션 -> 클라&서버 저장, 보안 강화, 느림, 브라우저 종료하면 삭제
+    5 : session_start()
+    6 : $_SESSION
+    7 : unset()
+    8 : location.href
+  */
+?>
